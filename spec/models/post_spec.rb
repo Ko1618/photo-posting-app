27f60730b42c0ebe00bd6aecfd9322d01f6f2677
takumi555 <htaaa838317@gmail.com>
2020-12-31 17:21:02 +0900

    commentのテスト（内容が保存されているかどうか）

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let!(:user) { create(:user) }
-  let!(:post) { create(:post, user: user) }
+  
 
   context '写真と内容が入力されている場合' do
+    let!(:post) { create(:post, user: user) }
     it '写真を保存できる' do 
       expect(post).to be_valid
     end
