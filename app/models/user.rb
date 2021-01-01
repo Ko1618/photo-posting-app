12c04afdd431c5e43ef4a74083a81577ae319562
takumi555 <htaaa838317@gmail.com>
2021-01-01 18:34:56 +0900

    Revert "Revert "環境変数の設定""
    This reverts commit 13cdc51ed5deed62ac02b9d79a902ec0cef0abb3.

---
+++ app/models/user.rb
@@ -0,0 +1,75 @@
+class User < ApplicationRecord
+  validates :username, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one :profile, dependent: :destroy
+  
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def hasliked?(post)
+    likes.exists?(post_id: post.id)
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'Ellipse.png'
+    end
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
+
+  def follow!(user)
+   
+    user_id = get_user_id(user)
+
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def following_count
+    followings.count
+  end
+
+  def follower_count
+    followers.count
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
+  end
+
+
+end
---
+++ app/serializers/comment_serializer.rb
@@ -0,0 +1,5 @@
+class CommentSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :content
+
+  belongs_to :user
+end
