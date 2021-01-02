03ef9740689ea0ea4895b204f7c95ba23fb7b985
takumi555 <htaaa838317@gmail.com>
2021-01-03 00:19:43 +0900

    投稿削除リンクの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, notice: '削除に成功しました'
+  end
+
   private
   def post_params
     params.require(:post).permit(:content, images: [])
